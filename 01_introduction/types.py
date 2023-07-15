ae7154d2759f671a43ca378cb7647e86115cd2da
Orland Contreras <orland102@gmail.com>
2023-07-15 18:42:09 -0500

    Fix some examples

--- 01_introduction/types.py
+++ 01_introduction/types.py
@@ -5,7 +5,8 @@ print((50 - 5 * 6) / 4)  # division always returns a floating point number
 print(8 / 5)  # division always returns a floating point number
 print(17 / 3)  # division always returns a floating point number
 print(17 // 3)  # floor division discards the fractional part
-print(17 % 3)  # the % operator returns the remainder of the division
+print(-17 // 3)
+print(17 % 3)  # the % operator returns the remainder (module) of the division
 print(5 * 3 + 2)  # result * divisor + remainder
 print(5**2)  # 5 squared
 print(2**7)  # 2 to the power of 7
