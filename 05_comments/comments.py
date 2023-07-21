9cb851711b146ef480a0bc591edf9262d62151bd
Orland Contreras <orland102@gmail.com>
2023-07-20 22:14:53 -0500

    Working with comments in da code

---
+++ 05_comments/comments.py
@@ -0,0 +1,13 @@
+# This program evaluates the hypotenuse c.
+# a and b are the lenghts of the legs.
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
+
+# Keyboard shortcut: CTRL + / (Windows) or CMD + / (Mac OS)
+# uncomment_me = 1
+# uncomment_me_too = 2
+# uncomment_me_also = 3
+
+print("") # Print uncomment variables
