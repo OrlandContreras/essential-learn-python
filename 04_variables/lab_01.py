ca7df941317f311cb54c449b642c344fca3a6eb7
Orland Contreras <orland102@gmail.com>
2023-07-20 22:00:44 -0500

    Simple defining variables practice

---
+++ 04_variables/lab_01.py
@@ -0,0 +1,12 @@
+# Create the variables below:
+john = 3
+mary = 5
+adam = 6
+
+print(john, mary, adam, sep=",")
+
+# Create a new variable called "total_apples" equal to the sum of John, Mary and Adam's apples:
+total_apples = john + mary + adam
+
+print(total_apples)
+
---
+++ 04_variables/lab_02.py
@@ -0,0 +1,16 @@
+# Resolve this equation: 3x^3 - 2x^2 + 3x - 1
+
+x = 0
+x = float(x)
+y = 3 * x ** 3 - 2 * x ** 2 + 3 * x - 1
+print("y = ", y)
+
+x = 1
+x = float(x)
+y = 3 * x ** 3 - 2 * x ** 2 + 3 * x - 1
+print("y = ", y)
+
+x = -1
+x = float(x)
+y = 3 * x ** 3 - 2 * x ** 2 + 3 * x - 1
+print("y = ", y)
