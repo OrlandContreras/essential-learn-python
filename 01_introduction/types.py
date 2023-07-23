6226f1e74f3d9d05e8915b46bde3c444ff4a0822
Orland Contreras <orland102@gmail.com>
2023-07-23 00:32:01 -0500

    Adding more example how we can work with types in python

--- 01_introduction/types.py
+++ 01_introduction/types.py
@@ -136,6 +136,7 @@ print(x[0][1])
 
 # Remove items from a list
 planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
 planets.pop()  # Goodbye, Pluto
 number_of_planets = len(planets)
 print("No, there are definitely", number_of_planets, "planets in the solar system.")
@@ -143,6 +144,29 @@ print("No, there are definitely", number_of_planets, "planets in the solar syste
 # Find items in a list
 jupiter_index = planets.index("Jupiter")
 print("Jupiter is the", jupiter_index + 1, "planet from the sun")
+
+# Use min() and max() on lists
+gravity_on_planets = [0.378, 0.907, 1, 0.377, 2.36, 0.916, 0.889, 1.12]
+bus_weight = 12650 # in kilograms, on Earth
+
+print("On Earth, a double-decker bus weighs", bus_weight, "kg")
+print("The lightest a bus would be in the solar system is", bus_weight * min(gravity_on_planets), "kg")
+print("The heaviest a bus would be in the solar system is", bus_weight * max(gravity_on_planets), "kg")
+
+# Join lists
+amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
+galilean_moons = ["Io", "Europa", "Ganymede", "Callisto"]
+
+regular_satellite_moons = amalthea_group + galilean_moons
+print("The regular satellite moons of Jupiter are", regular_satellite_moons)
+
+# Sort lists
+amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
+galilean_moons = ["Io", "Europa", "Ganymede", "Callisto"]
+
+regular_satellite_moons = amalthea_group + galilean_moons
+regular_satellite_moons.sort()
+print("The regular satellite moons of Jupiter are", regular_satellite_moons)
 
 # First Steps Towards Programming
 a, b = 0, 1
