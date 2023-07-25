4e5f308112da9acf8db9b19803295a6cbd3db8ce
Orland Contreras <orland102@gmail.com>
2023-07-24 22:24:28 -0500

    Add new examples

--- 03_data_structures/dictionaries.py
+++ 03_data_structures/dictionaries.py
@@ -1,3 +1,4 @@
+# Introduction to Python Dictionaries
 tel = {"jack": 4098, "sape": 4139}
 tel["guido"] = 4127
 print(tel)
@@ -13,3 +14,16 @@ print("guido" in tel)
 print("jack" not in tel)
 
 print(dict([("sape", 4139), ("guido", 4127), ("jack", 4098)]))
+
+# Create a dictionary
+planet = {
+    "name": "Earth", 
+    "age": 4.5, 
+    "diamater": 12742
+}
+
+# Read dictionary values
+print(planet.get('name'))
+
+# Modify dictionary values
+planet.update({'name': 'Makemake'})
