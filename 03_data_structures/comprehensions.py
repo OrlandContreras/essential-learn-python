8e8962a2c9f13b905f05adcb3ec2a36a0a9b0048
Orland Contreras <orland102@gmail.com>
2023-08-02 19:51:03 -0500

    Adding more example for work with lists

--- 03_data_structures/comprehensions.py
+++ 03_data_structures/comprehensions.py
@@ -22,3 +22,14 @@ print(list(zip(*matrix)))
 
 a = {x for x in "abracadabra" if x not in "abc"}
 print(a)
+
+# Cube - a three-dimensional array (3x3x3)
+cube = [
+    [[":(", "x", "x"], [":)", "x", "x"], [":(", "x", "x"]],
+    [[":)", "x", "x"], [":(", "x", "x"], [":)", "x", "x"]],
+    [[":(", "x", "x"], [":)", "x", "x"], [":)", "x", "x"]],
+]
+
+print(cube)
+print(cube[0][0][0])  # outputs: ':('
+print(cube[2][2][0])  # outputs: ':)'
--- 03_data_structures/lists.py
+++ 03_data_structures/lists.py
@@ -9,7 +9,11 @@ print(fruits.index("banana"))
 print(fruits.index("banana", 4))  # Find next banana starting a position 4
 print(fruits.reverse())
 
+# Add items to a list
 fruits.append("grape")
+print(fruits)
+
+fruits.insert(1, "mango")  # insert at a given position
 print(fruits)
 
 fruits.sort()
@@ -51,3 +55,34 @@ print(a - b)  # letters in a but not in b
 print(a | b)  # letters in a or b or both
 print(a & b)  # letters in both a and b
 print(a ^ b)  # letters in a or b but not both
+
+# Sorting a list
+my_list = [3, 5, 1, 2, 4]
+
+for i in range(len(my_list)):
+    for j in range(i + 1, len(my_list)):
+        if my_list[i] > my_list[j]:
+            my_list[i], my_list[j] = my_list[j], my_list[i]
+
+print(my_list)
+
+# Working with slices
+list_1 = [1]
+list_2 = list_1[:]
+list_1[0] = 2
+print(list_1)
+print(list_2)
+
+my_list = [10, 8, 6, 4, 2]
+new_list = my_list[1:3]
+print(new_list)
+
+# Negative slices
+my_list = [10, 8, 6, 4, 2]
+new_list = my_list[-1:-3:-1]
+print(new_list)
+
+# del instruction
+my_list = [10, 8, 6, 4, 2]
+del my_list[1:3]
+print(my_list)
