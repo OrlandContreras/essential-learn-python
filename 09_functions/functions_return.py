1281e7cb66fa2db5d050d5a8fce7b760f8f963cb
Orland Contreras <orland102@gmail.com>
2023-08-10 13:24:17 -0500

    add another example of functions

--- 09_functions/functions_return.py
+++ 09_functions/functions_return.py
@@ -56,3 +56,15 @@ for i in range(1, 20):
     if is_prime(i + 1):
         print(i + 1, end=" ")
 print()
+
+
+def list_updater(lst):
+    upd_list = []
+    for elem in lst:
+        elem **= 2
+        upd_list.append(elem)
+    return upd_list
+
+
+foo = [1, 2, 3, 4, 5]
+print(list_updater(foo))
