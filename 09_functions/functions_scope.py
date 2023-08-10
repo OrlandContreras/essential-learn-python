14d4b79880bccde47cd15c1d2058c092253918dd
Orland Contreras <orland102@gmail.com>
2023-08-10 13:26:02 -0500

    Working with functions and scopes

---
+++ 09_functions/functions_scope.py
@@ -0,0 +1,6 @@
+"""
+The scope of a name (e.g., a variable name) is the part of a code where the name is properly recognizable.
+
+For example, the scope of a function's parameter is the function itself. 
+The parameter is inaccessible outside the function.
+"""
