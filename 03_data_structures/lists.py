a2b1663c009caaa54d5af78a9c64f002b7ba86d5
Orland Contreras <orland102@gmail.com>
2023-08-19 19:32:33 -0500

    Add new examples

--- 03_data_structures/lists.py
+++ 03_data_structures/lists.py
@@ -86,3 +86,29 @@ print(new_list)
 my_list = [10, 8, 6, 4, 2]
 del my_list[1:3]
 print(my_list)
+
+# Example 1:
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+to_find = 5
+found = False
+
+for i in range(len(my_list)):
+    found = my_list[i] == to_find
+    if found:
+        break
+
+if found:
+    print("Element found at index", i)
+else:
+    print("absent")
+
+# Example 2:
+drawn = [5, 11, 9, 42, 3, 49]
+bets = [3, 7, 11, 42, 34, 49]
+hits = 0
+
+for number in bets:
+    if number in drawn:
+        hits += 1
+
+print(hits)
