7a97e50fe042745af3f0a8a9bc760f6e29701099
Orland Contreras <orland102@gmail.com>
2023-08-19 20:19:58 -0500

    Add a new example

--- 03_data_structures/comprehensions.py
+++ 03_data_structures/comprehensions.py
@@ -33,3 +33,19 @@ cube = [
 print(cube)
 print(cube[0][0][0])  # outputs: ':('
 print(cube[2][2][0])  # outputs: ':)'
+
+# List in list
+EMPTY = "-"
+ROOK = "ROOK"
+board = []
+
+for i in range(8):
+    row = [EMPTY for i in range(8)]
+    board.append(row)
+
+board[0][0] = ROOK
+board[0][7] = ROOK
+board[7][0] = ROOK
+board[7][7] = ROOK
+
+print(board)
