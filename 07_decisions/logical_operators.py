161243edf50b70508ef621a77823128e1c1e5ef1
Orland Contreras <orland102@gmail.com>
2023-08-19 19:19:34 -0500

    Adding file with logical evaluation

---
+++ 07_decisions/logical_operators.py
@@ -0,0 +1,19 @@
+# Exercise 1
+x = 1
+y = 0
+
+z = ((x == y) and (x == y)) or not (x == y)
+print(not (z))
+
+# Exercise 2
+x = 4
+y = 1
+
+a = x & y
+b = x | y
+c = ~x  # tricky!
+d = x ^ 5
+e = x >> 2
+f = x << 2
+
+print(a, b, c, d, e, f)
